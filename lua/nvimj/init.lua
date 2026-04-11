c394a18f2205ebf7249518bb08af5e4badc845e5
Roman Kotelnikov <ext.roman.kotelnikov@grabtaxi.com>
2026-04-11 10:27:55 +0900

    WIP; does not seem work

--- lua/nvimj/init.lua
+++ lua/nvimj/init.lua
@@ -45,19 +45,7 @@ M.open_repl = function()
   vim.api.nvim_set_current_win(current_win)
 end
 
-M.send_to_repl = function()
-  while M.repl_job_id == 0 or not vim.api.nvim_buf_is_valid(M.repl_buf_nr) do
-    M.open_repl(config.command)
-  end
-
-  -- Get current line
-  local line = vim.api.nvim_get_current_line()
-
-  local cursor_pos = vim.api.nvim_win_get_cursor(0)
-  local line_idx = cursor_pos[1] - 1
-  -- Send to terminal job (append \n to execute)
-  vim.fn.chansend(M.repl_job_id, line .. "\n")
-
+local function apply_marks(line_idx)
   local mark_id = line_idx + 1
 
   vim.api.nvim_buf_set_extmark(0, ns_id, line_idx, 0, {
@@ -68,8 +56,9 @@ M.send_to_repl = function()
     sign_text = "»", -- Optional: put a sign in the gutter
     sign_hl_group = "String",
   })
+end
 
-
+local function scroll_terminal()
   local windows = vim.api.nvim_list_wins()
   for _, win in ipairs(windows) do
     if vim.api.nvim_win_get_buf(win) == M.repl_buf_nr then
@@ -80,6 +69,45 @@ M.send_to_repl = function()
       vim.api.nvim_win_set_cursor(win, { line_count, 0 })
     end
   end
+end
+
+M.send_to_repl = function(type)
+  print("here")
+  while M.repl_job_id == 0 or not vim.api.nvim_buf_is_valid(M.repl_buf_nr) do
+    M.open_repl()
+  end
+
+  local start_mark = vim.api.nvim_buf_get_mark(0, "[")
+  local end_mark = vim.api.nvim_buf_get_mark(0, "]")
+
+  local start_line = start_mark[1] - 1
+  local end_line = end_mark[1]
+
+  -- For 'char' motions (like 'iw'), we might want just the words.
+  -- But for REPLs, 'line' is usually safer.
+  local lines = vim.api.nvim_buf_get_lines(0, start_line, end_line, false)
+  local text = table.concat(lines, "\n") .. "\n"
+
+  -- Send to terminal job (append \n to execute)
+  vim.fn.chansend(M.repl_job_id, text)
+
+  scroll_terminal()
+
+  for i = start_line, end_line - 1 do
+    apply_marks(i)
+  end
+end
+
+M.send_motion = function()
+  print("HERE")
+    -- Set the operatorfunc to our Lua function
+    -- Note: We use a global wrapper because operatorfunc traditionally 
+    -- expects a string pointing to a globally accessible function.
+  _G.repl_op_func = M.send_to_repl
+  vim.go.operatorfunc = "v:lua.repl_op_func"
+
+  -- Return the keys that trigger the operator
+  return "g@"
 end
 
 return M
